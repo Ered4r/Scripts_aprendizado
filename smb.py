6f7a829a5da7d3d737c5649d03cb126ad0303183
Ered4r <maphf99@gmail.com>
2025-02-12 19:59:17 -0300

    Add files via upload

--- smb.py
+++ smb.py
@@ -1,23 +1,48 @@
 import subprocess
+import json
+import sys
 
-def smb_vuln(host):
+def smb_vuln(host, port):
+    detected = False
+    details = ""
 
-	result = subprocess.run(
-		f"smbclient -L //{host} -U 'Convidado' -N 2>/dev/null | awk '{{if ($0 ~ /listing/) print $2}}' ",
-		shell=True, capture_output=True, text=True
-	)
+    try:
+        # Execute o comando e captura a saída
+        result = subprocess.run(
+            ["smbclient", "-L", f"//{host}/", "-p", f"{port}", "-N"],
+            capture_output=True, text=True, timeout=10
+        )
 
-	return bool(result.stdout.strip())
+        # Confere se a saída contém a string 'Sharename'
+        if "Sharename" in result.stdout:
+            detected = True
+            details = "SMB vulneravel"
+        else:
+            details = "SMB seguro"
+
+    except subprocess.TimeoutExpired:
+        details = f"Timeout ao tentar se conectar com o host: {host}."
+    except subprocess.CalledProcessError as e:
+        details = f"Erro ao executar o comando smbclient: {e}"
+    except FileNotFoundError:
+        details = "Comando smbclient não encontrado."
+
+    # Retorna o resultado como dicionário
+    result = {
+        'detected': detected,
+        'details': details,
+    }
+
+    return result
 
 if __name__ == "__main__":
-	import sys
+    if len(sys.argv) < 3:
+        print("Modo de uso: python smb.py <hostname_ou_ip> <porta>")
+        sys.exit(1)
 
-	if len(sys.argv) < 2:
-		print("Usage: <IP ADRESS>")
-		sys.exit(1)
+    host = sys.argv[1]
+    port = sys.argv[2]
+    result = smb_vuln(host, port)
 
-	host = sys.argv[1]
-	if smb_vuln(host):
-		print("Vulnerable")
-	else:
-		print("Safe")
+    # Exibe o resultado como JSON
+    print(json.dumps(result))
